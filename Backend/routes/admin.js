810042bae8627592818f2c62036b28a15a412c1d
therishikapadia <113783744+therishikapadia@users.noreply.github.com>
2024-12-19 19:38:15 +0530

    full final code

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -46,7 +46,6 @@ router.route('/inventory')
     .get(handleGetInventory)
     .delete(handleDeleteInventory);
 
-router.get('/inventory/search',handleSearchInventory);
 
 // Categories routes
 router.route('/categories')
